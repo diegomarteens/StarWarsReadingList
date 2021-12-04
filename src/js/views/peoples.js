0f346cade74e117ceeebadd510667b9216d83535
diegomarteens <diegomarteens@gmail.com>
2021-12-04 17:58:50 +0000

    ímgsUpdate

--- src/js/views/peoples.js
+++ src/js/views/peoples.js
@@ -2,14 +2,25 @@ import React from "react";
 import useSWR from "swr";
 import { useFavorites } from "../store/favorites";
 
-const placeholder = "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"
-const images ={
-	"1": 
-		"https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-large-heroes-page-luke-skywalker-16x9-xl.jpg.adapt.crop1x1.320w.jpg";
-	"2": 
-	"https://static.wikia.nocookie.net/canon-star-wars/images/5/56/ThreepioTFA-Fathead_%281%29.png/revision/latest?cb=20151205134916";
+const placeholder =
+	"https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg";
+const images = {
+	"1":
+		"https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-large-heroes-page-luke-skywalker-16x9-xl.jpg.adapt.crop1x1.320w.jpg",
+	"2":
+		"https://www.hollywoodmegastore.com/pub/media/catalog/product/cache/c9e0b0ef589f3508e5ba515cde53c5ff/2/9/2973_c3po_swix_34.jpg",
+	"3":
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9E5XnCLCpdyCdIHvk4sU73Ukq4Xafd9n4PGM8z-zFohijdn-Dfkgq4gXD60_iM0_LCtU&usqp=CAU",
+	"4": "https://cdn.singulart.com/artworks/v2/cropped/5877/main/fhd/713867_6ed990ab826a4975c232711091e55cdc.jpeg",
 
-}
+	"5": "https://i.pinimg.com/originals/89/23/8d/89238d20994113e9a40351eb0516f051.jpg",
+
+	"6":
+		"https://static.wikia.nocookie.net/frstarwars/images/a/a2/Owen_Lars.jpg/revision/latest/top-crop/width/360/height/450?cb=20160325202556",
+	"7": "https://i.pinimg.com/236x/7d/0c/5b/7d0c5b99034033a8d50548a7aa1e64b1.jpg",
+	"8": "",
+	"9": ""
+};
 export async function starWarsApiFetch(path) {
 	const response = await fetch("https://www.swapi.tech" + path);
 	if (response.status === 200) {
@@ -33,11 +44,7 @@ export function Characters() {
 							return (
 								<div className="col-3 col-xs-6" key={index}>
 									<div className="card">
-										<img
-											src={image[item.id] || placeholder }
-											className="card-img-top card-img-top"
-											alt="..."
-										/>
+										<img src={images[item.uid]} className="card-img-top card-img-top" alt="..." />
 										<div className="card-body">
 											<h5 className="card-title">{item.name}</h5>
 
